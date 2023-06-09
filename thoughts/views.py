69c5845cf7bd7c32a216f707ddd80bb379f64018
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-06-09 23:55:30 +0530

    feat: add function to show post with comments

--- thoughts/views.py
+++ thoughts/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Case, Value, When, CharField
+from django.db.models.functions import Concat
+from django.shortcuts import redirect, render
+from django.urls import reverse
+import random
 
-from .models import Thought
+from .models import Thought, Comment, Anonymous_Profile
 
 # Create your views here.
 anon_names = {
@@ -27,14 +32,65 @@ def generate_anon_name(thought_id):
 def create_post(request):
     if request.method == "POST":
         content = request.POST['thought']
-        anonymous = request.POST.get('anonymous')
+        anonymous = True if request.POST.get('anonymous') else False
         current_user = request.user
 
-        thought = Thought(content = content, author = current_user)
+        thought = Thought.objects.create(content = content, author = current_user, anonymous = anonymous)
+
         if anonymous:
-            thought.anonymous = True
+            anon_name = generate_anon_name(thought.id)
+            anon_profile = Anonymous_Profile.objects.create(
+                author = current_user, 
+                linked_post = thought, 
+                anonymous_name = anon_name
+                )
+    
+        return redirect('/')
+
+    return redirect('/404')
+
+@login_required
+def show_post(request, id):
+    try:
+        current_user  = request.user
+        # fetch the thought with the specific id
+        thought = Thought.objects.filter(id = id).annotate(
+        username = Case(
+            When(anonymous=True, then=Concat(Value('Anonymous '), 'anonymous_profile__anonymous_name')),
+            When(anonymous=False, then='author__username'),
+            output_field=CharField(),
+        ),).values('id', 'username', 'content', 'date_time')
+
+        # fetch all the anonymous profile associated with the post
+        anon_profile_associated = Anonymous_Profile.objects.filter(linked_post = id).values('author__username', 'anonymous_name')
+        anon_profiles = {}
+        # link author names with the anonymous name alloted
+        for data in anon_profile_associated:
+            anon_profiles[data['author__username']] = data['anonymous_name']
+
+        # fetch all the comments for the specific post
+        # with all the confidential information
+        raw_comments = Comment.objects.filter(linked_post = id).annotate(
+            can_delete=Case(
+            When(author=current_user, then=Value('True')),
+            default=Value('False'),
+            output_field=CharField()
+            ),).values('id', 'content', 'date_time', 'author__username', 'anonymous','can_delete')
         
-        thought.save()
+        comments = []
+        for comment in raw_comments:
+            # if user choose to be anonymous
+            # use the anon name that has been allocated to them
+            if comment['anonymous']:
+                username = 'Anonymous ' + anon_profiles[comment['author__username']]
+            else:
+                username = comment['author__username']
+            
+            comments.append({'id': comment['id'], 
+                             'content': comment['content'], 
+                             'date_time': comment['date_time'], 
+                             'username': username, 
+                             'can_delete': comment['can_delete']})
 
         return render(request, 'thoughts/index.html', {'thought': thought[0], 'comments': comments})
     except:
