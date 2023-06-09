69c2e493b7ebf5b8e6119294768bfa4c6e4bf38d
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-06-09 11:24:51 +0530

    feat: show all the posts on the landing_page

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,4 +1,26 @@
+from django.db.models import Case, Value, When, CharField
 from django.shortcuts import render
 
+from thoughts.models import Thought
+
 def index(request):
-    return render(request, 'landing_page/index.html')
+    current_user = request.user
+    all_thoughts = {}
+
+    if current_user.is_authenticated:
+        all_thoughts = Thought.objects.exclude(status__in = ['2', '3']).annotate(
+        username=Case(
+            When(anonymous=True, then=Value('Anonymous')),
+            When(anonymous=False, then='author__username'),
+            output_field=CharField()
+        )
+    ).annotate(
+        can_delete=Case(
+            When(author=current_user, then=Value('True')),
+            default=Value('False'),
+            output_field=CharField()
+        )
+    ).values('id', 'username', 'content', 'date_time', 'can_delete').order_by('-date_time')
+
+
+    return render(request, 'landing_page/index.html', {'thoughts': all_thoughts})
