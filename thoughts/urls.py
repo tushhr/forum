f153b1a8ae8d9b3a8dc405e861073d5160ab9171
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-06-09 23:56:20 +0530

    feat: add urls for adding comment, deleting post, and showing post

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:id>', views.show_post),
+    path("<int:id>/comment", views.write_comment),
+    path("delete/post/<int:id>", views.delete_post),
     path("post", views.create_post),
 ]
