ba1e6e6eafd743a059216dbbf3622dad5e11019a
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-06-10 00:51:47 +0530

    feat: add page to show the post

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path("<int:id>/comment", views.write_comment),
     path("delete/post/<int:id>", views.delete_post),
     path("post", views.create_post),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
