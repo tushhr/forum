918be3383112a5dca2cf01101bf3b97dfe08ffff
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-06-10 13:01:41 +0530

    fix: nits - styling of anchor tag, removing extra lines, adding styles to urls

--- forum/urls.py
+++ forum/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
+
 
 urlpatterns = [
     path('', include('landing_page.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('account/', include('accounts.urls')),
     path('thought/', include('thoughts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
